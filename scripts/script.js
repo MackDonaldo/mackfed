fe19a934852676ad2fb97147d437b7b70678c72a
MackDonaldo <90244265+MackDonaldo@users.noreply.github.com>
2021-10-05 23:02:05 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,6 @@
 // JavaScript Document
-var hamburgerMenu = document.querySelector("header i:first-of-type");
+// Hieronder halen we de benodigde elementen op waar we later een class op gaan zetten.
+var hamburgerMenu = document.querySelector("header button");
 var deNav = document.querySelector("section:first-of-type");
 var deHeader = document.querySelector("header");
 var deContent = document.querySelector("section:nth-of-type(2)");
@@ -7,10 +8,13 @@ var deFooter = document.querySelector("footer");
 var deArticle = document.querySelector("article")
 var overviewDropdown = document.querySelector("article span");
 
+// Hier zeggen we dat bij het klikken op de opgehaalde elementen, de onderstaande functies uit moeten worden gevoerd.
 hamburgerMenu.addEventListener("click", toggleMenu);
 overviewDropdown.addEventListener("click", toggleDropdown);
 
+// Deze function zet bij klikken van het hamburgermenu de class 'toonMenu' op de bovenstaande met querySelector geselecteerde elementen aan- en uit.
 function toggleMenu() {
+    hamburgerMenu.classList.toggle("menuOpen");
     deNav.classList.toggle("toonMenu");
     deHeader.classList.toggle("toonMenu");
     deContent.classList.toggle("toonMenu");
@@ -18,7 +22,7 @@ function toggleMenu() {
     deArticle.classList.toggle("toonMenu")
 }
 
+// Deze function zorgt ervoor dat er een klass op de variable overviewDropdown word gezet.
 function toggleDropdown() {
     overviewDropdown.classList.toggle("toonDropdown");
 }
-
